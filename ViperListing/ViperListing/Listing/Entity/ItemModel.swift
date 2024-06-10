95664a96f58eee9a5059e9a7eda8a39f22792fea
TheJuniorD3veloper <breakwithlimits@gmail.com>
2024-06-11 03:21:54 +0530

    viper example

---
+++ ViperListing/ViperListing/Listing/Entity/ItemModel.swift
@@ -0,0 +1,13 @@
+//
+//  ItemModel.swift
+//  ViperListing
+//
+//  Created by TheJunirD3v on 10/06/24.
+//
+
+import Foundation
+
+struct Item: Decodable {
+    let id: Int
+    let name: String
+}
